9723141fbc5726eba51fe6752458048fe9ff8713
Naafi <naafi96@gmail.com>
2020-01-12 16:24:15 +0530

    Education detail update

--- Resume_Website_React JS/src/sections/Education.js
+++ Resume_Website_React JS/src/sections/Education.js
@@ -6,8 +6,8 @@ class Education extends Component {
         return (
             <div className="condiv">
             <h1 className="subtopic">My Education</h1>
-            <Widecard title="commitee member" where="Ecell vit" from="March 2018" to="Present"/>
-            <Widecard title="Facilitator" where="Developer circle from facebook" from="December 2019" to="Present"/>
+            <Widecard title="M.Tech Software Engineering" where="VIT University" from="July 2017" to="Present"/>
+            <Widecard title="SSLC | HSC" where="Islamiah Higher Secondary School" from="2011" to="2017"/>
             </div>
             )
         }
