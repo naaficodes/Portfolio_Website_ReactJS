8e885a3448db8e811ddb35b459f9d4d01146a201
kdgyimah <agyemangkingsley21@gmail.com>
2020-01-28 13:53:19 +0000

    Fix compile warnings

--- Portfolio_Website_React JS/src/contents/Home.js
+++ Portfolio_Website_React JS/src/contents/Home.js
@@ -9,7 +9,7 @@ class Home extends Component {
     render() {
         return (
             <div className="condiv home">
-            <img src={profilepic} className="profilepic"></img>
+            <img src={profilepic} alt="ProfilePic" className="profilepic"></img>
             <ReactTypingEffect className="typingeffect" text={['I am Abdul Wahid Naafi','I am a web developer']} speed={100} eraseDelay={700}/>
             <Social />
             </div>
