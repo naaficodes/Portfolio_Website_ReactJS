db0d8ae0bb0d74f3fd739684770ef8b1db021fc4
daluclemas <chidaluanukam@gmail.com>
2022-02-24 17:43:39 -0800

    updated react router dom and changed the class attribute to className.

--- Portfolio_Website_React JS/src/contents/Skills.js
+++ Portfolio_Website_React JS/src/contents/Skills.js
@@ -14,8 +14,8 @@ class Skills extends Component {
             <div className="condiv skills">
             <h1 className="subtopic">My Skills</h1>
             <ul>
-            {this.state.myskills.map((value)=>{
-                return <li>{value}</li>
+            {this.state.myskills.map((value,index)=>{
+                return <li key={index}>{value}</li>
             })}
             </ul>
             </div>
