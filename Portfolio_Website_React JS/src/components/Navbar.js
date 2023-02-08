f20c4a084677e4eface110ed1f589064bbe79730
milliorn <scottmilliorn@gmail.com>
2023-02-08 15:39:31 -0800

    revert classNameList to classList

--- Portfolio_Website_React JS/src/components/Navbar.js
+++ Portfolio_Website_React JS/src/components/Navbar.js
@@ -12,10 +12,10 @@ class Navbar extends Component {
     activeitem=(x)=>
     {
         if(this.state.NavItemActive.length>0){
-            document.getElementById(this.state.NavItemActive).classNameList.remove('active');
+            document.getElementById(this.state.NavItemActive).classList.remove('active');
         }
         this.setState({'NavItemActive':x},()=>{
-            document.getElementById(this.state.NavItemActive).classNameList.add('active');
+            document.getElementById(this.state.NavItemActive).classList.add('active');
         });
     };
     render() {
