e692d53fabbd7635926bbbb5c37d356f40d36aa0
milliorn <scottmilliorn@gmail.com>
2023-02-08 15:02:38 -0800

    fixed Invalid DOM property `class`

--- Portfolio_Website_React JS/src/components/Social.js
+++ Portfolio_Website_React JS/src/components/Social.js
@@ -3,13 +3,13 @@ import React, { Component } from 'react';
 class Social extends Component {
     render() {
         return (
-            <div class="social">
-            <a href="https://codepen.io/naafi" aria-label='codepen' target="_blank" rel="noopener noreferrer"><i class="fab fa-codepen"></i></a>
-            <a href="https://github.com/naaficodes" aria-label='github' target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a>
-            <a href="https://Instagram.com/iam_naafi" aria-label='Instagram' target="_blank" rel="noopener noreferrer"><i class="fab fa-instagram"></i></a>
-            <a href="https://www.facebook.com/abdul.w.naafi" aria-label='facebook' target="_blank" rel="noopener noreferrer"><i class="fab fa-facebook-f"></i></a>
-            <a href="https://Linkedin.com/in/naafi" aria-label='Linkedin' target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin-in"></i></a>
-            <a href="https://medium.com/@iam_naafi" aria-label='medium' target="_blank" rel="noopener noreferrer"><i class="fab fa-medium-m"></i></a>
+            <div className="social">
+            <a href="https://codepen.io/naafi" aria-label='codepen' target="_blank" rel="noopener noreferrer"><i className="fab fa-codepen"></i></a>
+            <a href="https://github.com/naaficodes" aria-label='github' target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></a>
+            <a href="https://Instagram.com/iam_naafi" aria-label='Instagram' target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram"></i></a>
+            <a href="https://www.facebook.com/abdul.w.naafi" aria-label='facebook' target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook-f"></i></a>
+            <a href="https://Linkedin.com/in/naafi" aria-label='Linkedin' target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin-in"></i></a>
+            <a href="https://medium.com/@iam_naafi" aria-label='medium' target="_blank" rel="noopener noreferrer"><i className="fab fa-medium-m"></i></a>
             </div>
             )
         }
--- Portfolio_Website_React JS/src/components/Widecard.js
+++ Portfolio_Website_React JS/src/components/Widecard.js
@@ -3,11 +3,11 @@ import React, { Component } from 'react'
 class Widecard extends Component {
     render() {
         return (
-            <div class="widecard">
-            <div class="compdet">
+            <div className="widecard">
+            <div className="compdet">
             <h3>{this.props.title}</h3>
-            <h4 class="secondtext">{this.props.where}</h4>
-            <h4 class="secondtext">{this.props.from} - {this.props.to}</h4>
+            <h4 className="secondtext">{this.props.where}</h4>
+            <h4 className="secondtext">{this.props.from} - {this.props.to}</h4>
             </div>
             </div>
             )
